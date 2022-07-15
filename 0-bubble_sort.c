8913d96c3742b550a4004b6d339573798764e3f7
odelolajosh <oladapoodelola@gmail.com>
2022-07-15 22:14:43 +0100

    [Fix] - fix sort for array < 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp, flag = 1;
 
-	if (size <= 2)
+	if (size < 2)
 		return;
 
 	for (i = 0; i < size && flag; i++)
 	{
-		for (j = 0, flag = 0; j < size - 1; j++)
+		for (j = 0, flag = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
