9be09016f73267262b30bc798fa75905a89d1c42
odelolajosh <oladapoodelola@gmail.com>
2022-07-17 00:33:18 +0100

    [Fix] - only sort array with size > 2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -90,5 +90,8 @@ void _quicksort(int **array, size_t left, size_t right, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
+	if (size < 2)
+		return;
+
 	_quicksort(&array, 0, size - 1, size);
 }
