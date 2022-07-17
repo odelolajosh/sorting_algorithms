cce5dada1f262a5b99bd14bba9708236455bab1d
odelolajosh <oladapoodelola@gmail.com>
2022-07-17 01:06:31 +0100

    [Feature] - task 100

---
+++ 100-shell_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ * Knuth sequence: n+1 = n * 3 + 1
+ * 1, 4, 13, 40, 121, ...
+ * @array: given array
+ * @size: size
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, h;
+	int temp, didSwap = 0;
+
+	/* get initial maximum gap (h) */
+	h = 1;
+	while (h < size)
+		h = h * 3 + 1;
+
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+		{
+			didSwap = 0;
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+			{
+				temp = array[j];
+				array[j] = array[j - h];
+				array[j - h] = temp;
+				didSwap = 1;
+			}
+		}
+		/* decrease h */
+		h /= 3;
+		if (didSwap)
+			print_array(array, size);
+	}
+}
